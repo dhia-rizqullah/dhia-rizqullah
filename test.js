6fcb98c3a4aff9e46b1e8250605119707cf02c5b
dhiamonkey <dhiamuhammad@gmail.com>
2023-01-11 18:18:36 +0700

    test commit

---
+++ test.js
@@ -0,0 +1,11 @@
+// make javascript function that takes a string and makes it all uppercase
+
+function makeUpperCase(str) {
+  return str.toUpperCase();
+}
+
+// validate that the function works
+console.log(makeUpperCase("hello"));
+
+// export the function es6 style
+export default makeUpperCase;
